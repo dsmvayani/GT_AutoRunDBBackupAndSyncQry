b1edeeaa754808bc99ea7110ff53095c9b3cad52
AshrafShakeel <ashraf.shakeel@gentecsoft.com>
2025-10-15 16:34:31 +0500

    M Ashraf

--- GT_AutoRunDBBackupAndSyncQry/Form1.Designer.cs
+++ GT_AutoRunDBBackupAndSyncQry/Form1.Designer.cs
@@ -87,7 +87,7 @@
             label2.Name = "label2";
             label2.Size = new Size(75, 15);
             label2.TabIndex = 2;
-            label2.Text = "Version: 1.0.1";
+            label2.Text = "Version: 1.0.2";
             // 
             // DataDownloadBtn
             // 
